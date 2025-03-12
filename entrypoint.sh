d1555bcaca50eaa3ddd3cc7bd0e8921afccd03b1
Pieter-Jan van Dolderen <p.j.vandolderen@student.tudelft.nl>
2025-03-12 14:00:04 +0100

    update entrypoint for cbfpy with ros2

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,13 +6,20 @@ echo "source /opt/ros/humble/setup.bash" >> ~/.bashrc
 echo "source /ros_ws/install/setup.bash || true" >> ~/.bashrc
 source ~/.bashrc
 
-# Activate pyenv
+# Explicitly initialize pyenv
 export PYENV_ROOT="/root/.pyenv"
-export PATH="$PYENV_ROOT/bin:$PYENV_ROOT/shims:$PATH"
+export PATH="$PYENV_ROOT/bin:$PYENV_ROOT/shims:$PYENV_ROOT/bin:$PATH"
 eval "$(pyenv init --path)"
 eval "$(pyenv virtualenv-init -)"
 
+# Manually reload pyenv to ensure virtualenvs are recognized
+export PATH="$PYENV_ROOT/shims:$PYENV_ROOT/bin:$PATH"
+eval "$(pyenv init -)"
+eval "$(pyenv virtualenv-init -)"
+
 # Activate cbfpy virtual environment
-pyenv activate cbfpy-env
+pyenv exec python --version  # Debugging: Check if pyenv is working
+pyenv versions  # Debugging: Check if the virtual environment exists
+pyenv activate cbfpy-env  # Activate the virtual environment
 
 exec "$@"
