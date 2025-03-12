2f4c532b0c52f49e4eeb51453b1b5139ccf4ff9d
Pieter-Jan van Dolderen <p.j.vandolderen@student.tudelft.nl>
2025-03-12 13:40:01 +0100

    Change entrypoint.sh for pyenv activation

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,4 +6,13 @@ echo "source /opt/ros/humble/setup.bash" >> ~/.bashrc
 echo "source /ros_ws/install/setup.bash || true" >> ~/.bashrc
 source ~/.bashrc
 
+# Activate pyenv
+export PYENV_ROOT="/root/.pyenv"
+export PATH="$PYENV_ROOT/bin:$PYENV_ROOT/shims:$PATH"
+eval "$(pyenv init --path)"
+eval "$(pyenv virtualenv-init -)"
+
+# Activate cbfpy virtual environment
+pyenv activate cbfpy-env
+
 exec "$@"
