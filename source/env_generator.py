Add safety margin calculation (bug for nan values for true state)

source/env_generator.py
@@ -14,6 +14,7 @@ import random
 from obstacles import RectangleObstacle, CircleObstacle
 import numpy as np
 import os
+from uncertainty_costmap import UncertaintyCostmap
 
 
 class EnvGeneratorConfig:
@@ -222,6 +223,12 @@ class EnvGenerator:
             cbf_reduction=self.config.cbf_reduction
         )
 
+        uncertainty_costmap = UncertaintyCostmap(
+            cbf=cbf,
+            min_values_state=np.array([-10, -10, -5, -5]),
+            max_values_state=np.array([10, 10, 5, 5])
+        )
+
         # generate the planner
         planners = {}
         if self.config.cbf_infused_a_star:
@@ -275,7 +282,7 @@ class EnvGenerator:
             )
             visualizers[key] = visualizer
         
-        return env, visualizers, config, cbf, planners, cbf_costmap
+        return env, visualizers, config, cbf, planners, cbf_costmap, uncertainty_costmap
     
     def _run_planner(self, 
                      env: RobotBaseEnv, 
@@ -284,6 +291,7 @@ class EnvGenerator:
                      cbf: CBF, 
                      planner: AStarPlanner | CBFInfusedAStar, 
                      cbf_costmap: CBFCostmap, 
+                     uncertainty_costmap: UncertaintyCostmap,
                      env_folder: str):
         max_timesteps = env.fps * self.config.max_duration_of_simulation
         timesteps = 0
@@ -301,7 +309,6 @@ class EnvGenerator:
             pos_des = env.get_desired_state()
             
             # calculate nominal control
-            # nominal_control = pd_controller(pos_des, current_state[:2], current_state[2:])
             nominal_control = pd_controller(pos_des, estimated_state[:2], estimated_state[2:])
             
             # safe data for visualizer  
@@ -313,7 +320,11 @@ class EnvGenerator:
             visualizer.data['robot_pos_estimated'].append(estimated_state[:2])
 
             # apply safety filter
-            # u = cbf.safety_filter(current_state, nominal_control)
+            safety_margin = uncertainty_costmap.calculate_safety_margin(
+                epsilon=0.4,
+                u_nominal=nominal_control,
+                mode='robust'
+            )
             u = cbf.safety_filter(estimated_state, nominal_control)
 
             # safe control data for visualizer
@@ -378,7 +389,7 @@ class EnvGenerator:
 
     @logger.catch
     def _run_env(self, env_id):
-        env, visualizers, config, cbf, planners, cbf_costmap = self._generate_env_elements()
+        env, visualizers, config, cbf, planners, cbf_costmap, uncertainty_costmap = self._generate_env_elements()
         success = {}
 
         # create folder for this simulation
@@ -411,6 +422,7 @@ class EnvGenerator:
                 cbf=cbf,
                 planner=planner,
                 cbf_costmap=cbf_costmap,
+                uncertainty_costmap=uncertainty_costmap,
                 env_folder=env_folder
             )
             success[planner_name] = goal_reached
@@ -469,8 +481,8 @@ def main():
     # cbf_mode 0: PD + CBF
     # cbf_mode 1: CLF + CBF
     config = EnvGeneratorConfig(
-        number_of_simulations=30,
-        fps=10,
+        number_of_simulations=1,
+        fps=50,
         min_number_of_obstacles=5,
         max_number_of_obstacles=10,
         environment_size=(20, 20),

source/obstacles.py
@@ -111,14 +111,14 @@ class RectangleObstacle(Obstacle):
         costmap[row_min:row_max, col_min:col_max] = np.inf
         return costmap
 
-    def h(self, Z):
+    def h(self, z, safety_margin=0.0):
         # batched -> faster
-        if Z.ndim == 1:
-            Z = Z[None, :]  # Reshape to (1, 4)
+        if z.ndim == 1:
+            z = z[None, :]  # Reshape to (1, 4)
 
         # Z: shape (N, 4)
-        px = Z[:, 0]
-        py = Z[:, 1]
+        px = z[:, 0]
+        py = z[:, 1]
 
         closest_point = self.find_closest_point_to_obstacle(px, py)  # (N, 2)
 
@@ -126,7 +126,7 @@ class RectangleObstacle(Obstacle):
         norm = jnp.linalg.norm(vectors, axis=1, keepdims=True) + 1e-6
         normal_vectors = vectors / norm
 
-        h_values = jnp.sum(normal_vectors * vectors, axis=1) - self.robot.radius - self.robot.safety_margin
+        h_values = jnp.sum(normal_vectors * vectors, axis=1) - self.robot.radius - safety_margin
         return h_values  # shape (N,)
 
     def check_collision(self, robot: RobotBase, safety_margin=0.0):
@@ -203,19 +203,19 @@ class CircleObstacle(Obstacle):
                     costmap[min(max(row-r-1, 0), rows - 1), min(max(col+c, 0), cols - 1)] = np.inf
                     costmap[min(max(row-r-1, 0), rows - 1), min(max(col-c-1, 0), cols - 1)] = np.inf
         return costmap
-    
-    def h(self, Z):
-        # batched -> faster
-        if Z.ndim == 1:
-            Z = Z[None, :]  # Reshape to (1, 4)
-        
-        # Z: shape (N, 4)
-        px = Z[:, 0]
-        py = Z[:, 1]
+
+    def h(self, z, safety_margin=0.0):
+        if z.ndim == 1:
+            z = z[None, :]
+
+        px = z[:, 0]
+        py = z[:, 1]
         delta = jnp.stack([px, py], axis=1) - self.pos_center  # (N, 2)
 
-        h_values = jnp.sum(delta**2, axis=1) - (self.robot.radius + self.radius + self.robot.safety_margin)**2
-        return h_values  # shape (N,)
+        dist = jnp.linalg.norm(delta, axis=1)
+        buffer = self.robot.radius + self.radius + safety_margin
+        h_values = dist - buffer
+        return h_values # shape (N,)
 
     def pygame_drawing(self, screen, color):
         radius_px = int(self.env_config.pixels_per_meter[0] * self.radius)